b8d5678628eb5b4f70a2acfa94f3052248e821ee
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-04-27 16:22:32 +0530

    todosData.js

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Write a blog post on Building a ToDo App using React",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Build a ToDo App using React",
+        completed: true
+    },
+    {
+        id: 3,
+        text: "Computer Graphics Assignment",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Phone call with a client",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Download Money Heist Season 2",
+        completed: false
+    }
+]
+
+export default todosData
