608023bece5b4d73e274045e73b201570fbe54c4
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-04-27 16:18:57 +0530

    App.js

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react"
+import TodoItem from "./TodoItem"
+import todosData from "./todosData"
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
+    
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+        
+        return (
+            <div className="todo-list">
+                <h1> TO-DO LIST </h1>
+                <h3>Anjali, what's on your mind today?</h3>
+                {todoItems}
+            </div>
+        )    
+    }
+}
+
+export default App
